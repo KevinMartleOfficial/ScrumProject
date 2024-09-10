914d2ef968aaa35f367c7c56817eba4fcea1f5bf
Kenneth VDAB <kenneth.vangijsel@vdab.be>
2024-09-10 11:27:45 +0200

    Initial commit

---
+++ src/main/java/be/vdab/scrumjava202409/ScrumJava202409Application.java
@@ -0,0 +1,13 @@
+package be.vdab.scrumjava202409;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumJava202409Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrumJava202409Application.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/scrumjava202409/ScrumJava202409ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.scrumjava202409;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumJava202409ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
