f08b0841830ed1b1f200ffc2bcc1118e38d55580
Jelle <jelle.brg@outlook.com>
2024-09-17 14:50:13 +0200

    BES 1.1 merge into master

--- src/main/java/be/vdab/scrumjava202409/bestellingen/BestellingRepository.java
+++ src/main/java/be/vdab/scrumjava202409/bestellingen/BestellingRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public class BestellingRepository {
+class BestellingRepository {
     private final JdbcClient jdbcClient;
     public BestellingRepository(JdbcClient jdbcClient) {
         this.jdbcClient = jdbcClient;
@@ -14,13 +14,15 @@ public class BestellingRepository {
 
     public List<Bestelling> eerste5bestellingen() {
         String sql = """
-                select * from bestellingen
-                where bestellingsStatusId = 4
-                order by bestelId
+                select * from bestellingen 
+                inner join bestellingsstatussen bs on bestellingen.bestellingsStatusId = bs.bestellingsStatusId
+                         where bs.naam = 'Klaarmaken'
+                         order by bestelId
                 limit 5;
                 """;
-        return jdbcClient.sql(sql).query(Bestelling.class).list();
-        }
+        return jdbcClient.sql(sql).query(Bestelling.class).
+                stream().toList();
+    }
 
     public Bestelling findEersteBestellingMetStatusKlaarmaken() {
         var sql = """
@@ -38,15 +40,5 @@ public class BestellingRepository {
     long findAantalBestellingen() {
         String sql = "select count(*) from bestellingen";
         return jdbcClient.sql(sql).query(Long.class).single();
-    }
-
-    public Bestelling findById(long bestelId){
-        String sql = """
-                select * from bestellingen where bestelId = ?;
-                """;
-        return jdbcClient.sql(sql)
-                .param(bestelId)
-                .query(Bestelling.class)
-                .single();
     }
 }
