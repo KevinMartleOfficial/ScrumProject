86d6a6369c88593519b9dc6189294b1336f8e729
Kevin Martlé <kevin.martle@vdabcampus.be>
2024-09-25 12:54:12 +0200

    Lev 4.1 rdy for code review
    -Methode geschreven in MagazijnPlaatsRepository om aan een object Magazijn te geraken
    -Frontend krijgt een lijst van DTO's met artikelnaam, aantal, en een magazijnplaats(rek en rij samengevoegd)

--- src/main/java/be/vdab/scrumjava202409/inkomendeleveringslijnen/InkomendeLeveringsLijnController.java
+++ src/main/java/be/vdab/scrumjava202409/inkomendeleveringslijnen/InkomendeLeveringsLijnController.java
@@ -1,10 +1,7 @@
 package be.vdab.scrumjava202409.inkomendeleveringslijnen;
 
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +19,10 @@ public class InkomendeLeveringsLijnController {
     public void voegInkomendeLeveringsLijnenToe(@RequestBody @Valid List<InkomendeLeveringsLijnGeenMagazijnId> inkomendeLeveringsLijnList){
 
         inkomendeLeveringsLijnService.voegInkomendeLeveringsLijnenToe(inkomendeLeveringsLijnList);
+    }
+
+    @GetMapping("{inkomendeLeveringsId}")
+    public List<DTOArtikelNaamInkomendeLeveringsLijnAantalGoedgekeurdEnMagazijnPlaats>findAllInkomendeLeveringsLijnenByInkomendeLeveringsId(@PathVariable long inkomendeLeveringsId){
+        return inkomendeLeveringsLijnService.findAllInkomendeLeveringsLijnenByInkomendeLeveringsId(inkomendeLeveringsId);
     }
 }
