8b554b3ee261fe9ee206c7e27846e826f62f5f0f
Kevin Martlé <kevin.martle@vdabcampus.be>
2024-09-25 10:20:35 +0200

    Lev 1.1 rdy for code review

---
+++ src/main/java/be/vdab/scrumjava202409/leveranciers/LeverancierRepository.java
@@ -0,0 +1,37 @@
+package be.vdab.scrumjava202409.leveranciers;
+
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class LeverancierRepository {
+    private final JdbcClient jdbcClient;
+
+    public LeverancierRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    public long findLeveranciersIdByLeveranciersStukjeNaam(String stukjeNaam){
+        String sql = """
+                select leveranciersId
+                from leveranciers
+                where naam like ?
+                """;
+        return jdbcClient.sql(sql)
+                .param("%" + stukjeNaam + "%")
+                .query(Long.class)
+                .single();
+    }
+
+    public long findLeverancierIdByNaam(String naam){
+        String sql = """
+                select leveranciersId
+                from leveranciers
+                where naam = ?
+                """;
+        return jdbcClient.sql(sql)
+                .param(naam)
+                .query(Long.class)
+                .single();
+    }
+}
