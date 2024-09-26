6b7873814a3710fdc7d03492be8b7da1db5f6fc7
cursistJavaHeverlee <ben.neel@vdabcampus.be>
2024-09-26 12:27:46 +0200

    LEV-5.1 & LEV-5.2 & LEV-5.4

--- src/main/resources/static/js/leveringsBonOverzicht.js
+++ src/main/resources/static/js/leveringsBonOverzicht.js
@@ -2,10 +2,18 @@
 import {byId, setText, toon, verberg} from "./util.js";
 
 const leveringsBonId = JSON.parse(sessionStorage.getItem("inkomendeLeveringsId"));
+const leveringsbonNummer = JSON.parse(sessionStorage.getItem("leveringsbonNummer")); //moet nog gehaald worden uit session storage die nog niet is aangemaakt
+setText("leveringsbonNummer", "Leveringsbon nr: " /*+ leveringsbonNummer */); //Vandaar dat leveringsbonNummer hier nog tss commentaar staat.
 
-setText("leveringsBonId", leveringsBonId);
-
-byId("knop")
+byId("knop").onclick = async () => {
+    await fetch(`inkomendeleveringslijn/verhoog`, {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: leveringsBonId
+    });
+    sessionStorage.clear();
+    window.location = "./index.html";
+}
 
 const response = await fetch(`inkomendeleveringslijn/${leveringsBonId}`);
 
@@ -24,7 +32,35 @@ if (response.ok) {
         input.setAttribute("type", "checkbox");
         input.className = "checkboxes";
         tr.insertCell().appendChild(input);
+        checkboxLijstAanmaken();
     }
 } else {
     toon("storing");
+}
+
+function checkboxLijstAanmaken() {
+    const checkboxList = document.getElementsByClassName("checkboxes");
+
+    for (let i = 0; i < checkboxList.length; i++) {
+        checkboxList[i].onchange = () => {
+            telVinkjes();
+        }
+    }
+}
+
+function telVinkjes() {
+    const checkboxList = document.getElementsByClassName("checkboxes");
+    var aantalVinkjes = 0;
+
+    for (let i = 0; i < checkboxList.length; i++) {
+        if (checkboxList[i].checked) {
+            aantalVinkjes++;
+        }
+    }
+
+    if (aantalVinkjes === checkboxList.length) {
+        toon("knop");
+    } else {
+        verberg("knop");
+    }
 }
